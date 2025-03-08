207a664f631151058edfa1c5ed22771554ff698b
Itin <thenewera07@gmail.com>
2025-03-08 18:02:40 -0300

    adicionando novo projeto

--- views/PowerBi.py
+++ views/PowerBi.py
@@ -12,7 +12,7 @@ st.markdown(
 st.markdown(
     """
     <div style="text-align: center; margin-bottom: 10px;">
-        <p style="font-size: 16px; font-weight: bold;">Dashboard de Vendas de Carros:</p>
+        <p style="font-size: 32px; font-weight: bold;">Dashboard de Vendas de Carros:</p>
         <p style="font-size: 16px;"> 
         Utilizando DAX para calculos de métricas: YTD, MOM, YOY, MTD
         </p>
@@ -48,6 +48,54 @@ st.markdown(
         }}
         a:hover h3 {{
             color: #007BFF; /* Cor do texto ao passar o mouse */
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+## Segundo projeto Dashboard
+
+# Legenda acima do dashboard
+st.markdown(
+    """
+    <div style="text-align: center; margin-bottom: 10px;">
+        <p style="font-size: 32px; font-weight: bold;">Dashboard Análise de Vendas:</p>
+        <p style="font-size: 16px;"> 
+        Utilizando DAX para calculos de métricas: YTD, MOM, YOY, MTD
+        </p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+
+st.markdown(
+    """
+    <div style="display: flex; justify-content: center;">
+        <iframe title="Elertro Vendas" width="1000" height="450" src="https://app.powerbi.com/view?r=eyJrIjoiN2FkZWM3ZTUtOWNiOS00NTlhLWJlZjgtYTQ1YWU3NDc4ZWViIiwidCI6Ijk4YTQ3YjdhLTBjMzYtNGUyNy04MGE3LTZjMDU2YzdjMWI0NCJ9" frameborder="0" allowFullScreen="true"></iframe>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+bi_link = "https://app.powerbi.com/view?r=eyJrIjoiN2FkZWM3ZTUtOWNiOS00NTlhLWJlZjgtYTQ1YWU3NDc4ZWViIiwidCI6Ijk4YTQ3YjdhLTBjMzYtNGUyNy04MGE3LTZjMDU2YzdjMWI0NCJ9"
+
+st.markdown(
+    f"""
+    <div style="text-align: center; margin: 20px;">
+        <a href="{bi_link}" target="_blank" style="text-decoration: none;">
+            <img src="https://i.ibb.co/5h0Z4Nb3/dash-vendas.png" alt="Dashboard" style="width: 100%; max-width: 800px; transition: transform 0.3s;">
+            <h3 style="transition: color 0.3s;">Dashboard Análise de Vendas</h3>
+        </a>
+    </div>
+
+    <style>
+        a:hover img {{
+            transform: scale(1.05);
+        }}
+        a:hover h3 {{
+            color: #007BFF;
         }}
     </style>
     """,
