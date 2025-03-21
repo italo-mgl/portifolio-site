812cd2e7b83e5e3d926f1ffd83825f1b6683fff1
itin <thenewera07@gmail.com>
2025-03-21 17:40:59 -0300

    Atualizando portifolio

--- views/PowerBi.py
+++ views/PowerBi.py
@@ -8,51 +8,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Legenda acima do dashboard
-st.markdown(
-    """
-    <div style="text-align: center; margin-bottom: 10px;">
-        <p style="font-size: 32px; font-weight: bold;">Dashboard de Vendas de Carros:</p>
-        <p style="font-size: 16px;"> 
-        Utilizando DAX para calculos de métricas: YTD, MOM, YOY, MTD
-        </p>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
 
-# Iframe centralizado
-st.markdown(
-    """
-    <div style="display: flex; justify-content: center;">
-        <iframe title="Car_Sales" width="1000" height="450" src="https://app.powerbi.com/view?r=eyJrIjoiMDIyMWYxYjktODI5ZS00NWMxLWIxNTItNTg0MGEzMDBkZTcwIiwidCI6Ijk4YTQ3YjdhLTBjMzYtNGUyNy04MGE3LTZjMDU2YzdjMWI0NCJ9" frameborder="0" allowFullScreen="true"></iframe>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
-# bi_link_car = "https://app.powerbi.com/view?r=eyJrIjoiMDIyMWYxYjktODI5ZS00NWMxLWIxNTItNTg0MGEzMDBkZTcwIiwidCI6Ijk4YTQ3YjdhLTBjMzYtNGUyNy04MGE3LTZjMDU2YzdjMWI0NCJ9"
-
-# st.markdown(
-#     f"""
-#     <div style="text-align: center; margin: 20px;">
-#         <a href="{bi_link_car}" target="_blank" style="text-decoration: none;">
-#             <img src="https://i.ibb.co/jk7G24zn/dash-cars.png" alt="Dashboard" style="width: 100%; max-width: 800px; transition: transform 0.3s;">
-#             <h3 style="transition: color 0.3s;">Dashboard de Vendas de Carros</h3>
-#         </a>
-#     </div>
-
-#     <style>
-#         a:hover img {{
-#             transform: scale(1.05);
-#         }}
-#         a:hover h3 {{
-#             color: #007BFF; /* Cor do texto ao passar o mouse */
-#         }}
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
 
 ## Segundo projeto Dashboard
 
@@ -101,8 +57,78 @@ st.markdown(
 #     """,
 #     unsafe_allow_html=True
 # )
-
 ## Terceiro projeto Dashboard
+
+# Legenda acima do dashboard
+st.markdown(
+    """
+    <div style="text-align: center; margin-bottom: 10px;">
+        <p style="font-size: 32px; font-weight: bold;">Dashboard Financeiro:</p>
+        <p style="font-size: 16px;"> 
+        Utilizando DAX para calculos de métricas: Faturamento, Margem, Imposto, Lucro.
+        </p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+
+st.markdown(
+    """
+    <div style="display: flex; justify-content: center;">
+        <iframe title="Dashboard-Financeiro" width="1000" height="450" src="https://app.powerbi.com/view?r=eyJrIjoiNzg2MjBlMGMtZTg5YS00YWI1LWFiZGItOGI3ZDQ5MTA5MTYwIiwidCI6Ijk4YTQ3YjdhLTBjMzYtNGUyNy04MGE3LTZjMDU2YzdjMWI0NCJ9" frameborder="0" allowFullScreen="true"></iframe>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+# Legenda acima do dashboard
+st.markdown(
+    """
+    <div style="text-align: center; margin-bottom: 10px;">
+        <p style="font-size: 32px; font-weight: bold;">Dashboard de Vendas de Carros:</p>
+        <p style="font-size: 16px;"> 
+        Utilizando DAX para calculos de métricas: YTD, MOM, YOY, MTD
+        </p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+# Iframe centralizado
+st.markdown(
+    """
+    <div style="display: flex; justify-content: center;">
+        <iframe title="Car_Sales" width="1000" height="450" src="https://app.powerbi.com/view?r=eyJrIjoiMDIyMWYxYjktODI5ZS00NWMxLWIxNTItNTg0MGEzMDBkZTcwIiwidCI6Ijk4YTQ3YjdhLTBjMzYtNGUyNy04MGE3LTZjMDU2YzdjMWI0NCJ9" frameborder="0" allowFullScreen="true"></iframe>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+# bi_link_car = "https://app.powerbi.com/view?r=eyJrIjoiMDIyMWYxYjktODI5ZS00NWMxLWIxNTItNTg0MGEzMDBkZTcwIiwidCI6Ijk4YTQ3YjdhLTBjMzYtNGUyNy04MGE3LTZjMDU2YzdjMWI0NCJ9"
+
+# st.markdown(
+#     f"""
+#     <div style="text-align: center; margin: 20px;">
+#         <a href="{bi_link_car}" target="_blank" style="text-decoration: none;">
+#             <img src="https://i.ibb.co/jk7G24zn/dash-cars.png" alt="Dashboard" style="width: 100%; max-width: 800px; transition: transform 0.3s;">
+#             <h3 style="transition: color 0.3s;">Dashboard de Vendas de Carros</h3>
+#         </a>
+#     </div>
+
+#     <style>
+#         a:hover img {{
+#             transform: scale(1.05);
+#         }}
+#         a:hover h3 {{
+#             color: #007BFF; /* Cor do texto ao passar o mouse */
+#         }}
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+
+## Quarto projeto Dashboard
 
 # Legenda acima do dashboard
 st.markdown(
