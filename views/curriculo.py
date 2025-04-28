a8ccc2a21011bc5777e96f9543e89511bc273771
itin <thenewera07@gmail.com>
2025-04-28 09:36:54 -0300

    corrigindo sessão de curriculo

--- views/curriculo.py
+++ views/curriculo.py
@@ -14,53 +14,52 @@ st.write("""
 # Seção de Formação
 st.header("Formação")
 st.write("""
-- **UNIASSELVI**, Fortaleza, CE  
-  Análise e Desenvolvimento de Sistemas (Jan. 2022 -- Dez. 2024)
+- **UNIASSELVI** - Fortaleza, CE  
+  Análise e Desenvolvimento de Sistemas  
+  Jan. 2022 – Dez. 2024
 
-- **Universidade Federal do Ceará**, Fortaleza, CE  
-  Engenharia Agronômica (Ago. 2018 -- Dez. 2023)
+- **Universidade Federal do Ceará** - Fortaleza, CE  
+  Engenharia Agronômica  
+  Ago. 2018 – Dez. 2023
 """)
 
 # Seção de Experiência
 st.header("Experiência")
+
+st.subheader("Analista de Dados e Sistemas")
+st.write("Hostweb Data Center - Fortaleza, CE  |  Fev. 2024 – Atualmente")
 st.write("""
-- **Analista de Processos**  
-  Hostweb Data Center, Fortaleza, CE (Fev. 2024 -- Atualmente)  
-  - Mapeamento de processos com delineamento de escopo e criação de fluxogramas utilizando Bizagi, Sharepoint e Bitrix24.
-  - Documentação de processos com notação BPMN, analisando e buscando melhoria contínua dos processos de negócios com metodologia PDCA.
-  - Gerenciamento de CRM e funis de vendas, criando automações em cards de negócios na plataforma Bitrix24.
-
-- **Estagiário de Análise de Dados**  
-  Meireles e Freitas Advogados Associados, Fortaleza, CE (Jan. 2023 -- Dez. 2023)  
-  - Coleta, análise e tratamento de dados utilizando Excel e PowerBI e DAX.
-  - Criação de relatórios, dashboards e apresentações utilizando PowerBI e PowerPoint.
-  - Criação de automações de processos judiciais com Python utilizando Selenium e PyAutoGUI.
-
-- **Bootcamp Ciências de Dados**  
-  Instituto Atlântico, Fortaleza, CE (Out. 2022 -- Dez. 2022)  
-  - Bootcamp seguindo metodologias ágeis utilizadas no mercado com SCRUM e KANBAN.
-  - Fundamentação de estatística voltada para análise de dados.
-  - Utilização de bibliotecas Python para análise de dados como Pandas, Seaborn e Matplotlib.
-  - Criação de projeto sobre análise de sentimento do Twitter, definindo um sentimento de acordo com o contexto da mensagem e criando nuvem das palavras mais usadas.
+- Atuei com a equipe de processos para definir os escopos dos setores e a criação de métricas e KPIs da empresa, com a criação de dashboards no Power BI para cada time, além de realizar apresentações à diretoria e stakeholders.
+- Responsável por estruturar, implementar e dar suporte ao novo sistema ERP para o time de vendas varejo, além de criar relatórios e definir métricas analisando o desempenho do time de vendas, facilitando a gestão da diretoria e stakeholders.
+- Responsável por estruturar todo o estoque da empresa no sistema, partindo desde a coleta das informações nos bancos de dados SQL Server e PostgreSQL, com o tratamento e a modelagem dos dados feita em Excel e realizando a atualização dos itens no sistema via API com Python, corrigindo e estruturando o estoque.
+- Responsável por estruturar o CRM da empresa em conjunto com o setor comercial, com a criação de automações, fluxos de trabalho, e dashboards, otimizando o funil de vendas e facilitando o gerenciamento.
+- Atuei no tratamento, estruturação e modelagem dos dados dos clientes no processo de migração de sistema ERP, coletando dados via SQL e realizando a migração via API com scripts em Python.
+- Atuei com o time de marketing na criação e mensuração de campanhas para prospecção de leads, através de eventos e estratégias de inbound marketing, com foco em otimização de resultados e melhoria contínua.
+- Responsável por manter e dar suporte aos sistemas utilizados na empresa, como CRM comercial, ERP do time de varejo, ferramenta ITSM interna de chamados e tickets, além de criar automações via Power Automate, para disparo de questionários nas campanhas realizadas pelo RH.
 """)
 
-# Seção de Projetos
-st.header("Projetos")
+st.subheader("Estagiário de Análise de Dados")
+st.write("Meireles e Freitas Advogados Associados - Fortaleza, CE  |  Jan. 2023 – Dez. 2023")
 st.write("""
-- **Gitlytics** | *Python, Flask, React, PostgreSQL, Docker*  
-  Desenvolveu uma aplicação web full-stack usando Flask servindo uma API REST com React no front-end.
-
-- **Simple Paintball** | *Spigot API, Java, Maven, TravisCI, Git*  
-  Desenvolveu um plugin para servidor Minecraft que divertiu crianças durante o tempo livre, com mais de 2K downloads e uma média de 4.5/5 estrelas.
+- Atuei no time de advogados associados, sendo responsável por coletar, tratar e analisar os dados oriundos dos processos com a utilização de Excel e criação de relatórios e dashboards no Power BI com uso de DAX.
+- Contactava os diferentes times da empresa, atuando nas diversas etapas dos processos, além de participar de reuniões com os stakeholders para o direcionamento na criação de relatórios e dashboards.
+- Responsável por criar relatórios semanais, quinzenais e mensais para diferentes setores, além de realizar apresentações dos resultados para a diretoria com a utilização de Power BI e PowerPoint.
+- Criação de automações de processos judiciais com Python utilizando Selenium e PyAutoGUI.
 """)
 
-# Seção de Habilidades Técnicas
-st.header("Habilidades Técnicas")
+# Seção de Certificações e Habilidades
+st.header("Certificações e Habilidades")
 st.write("""
-- **Tecnologias**: PowerBI, DAX, Python, SQL, Excel, R, FastAPI, BPMN  
-- **Bibliotecas Python**: Pandas, Numpy, Matplotlib, Plotly, Scikit-Learn, Selenium, PyAutoGUI  
-- **Ferramentas**: Git, GitHub, Docker, Jupyter Notebook, VS Code, Bizagi, Bitrix24  
+- **Data Tech Florida:** Ciências de Dados e Inteligência Artificial
+- **Hashtag Treinamentos:** Análise de Dados Impressionadora
+- **IMPARH:** Inglês B2
 """)
 
-# Rodapé
-st.write("© Ítalo Magalhães. Todos os direitos reservados.")
+st.write("### Tecnologias:")
+st.write("Power BI, DAX, Python, SQL Server, PostgreSQL, Excel, DataBricks")
+
+st.write("### Ferramentas:")
+st.write("Power Automate, Git, GitHub, Jupyter Notebook, Bitrix24")
+
+st.write("### Bibliotecas Python:")
+st.write("Pandas, Numpy, Matplotlib, Plotly, Scikit-Learn, Selenium, PyAutoGUI")
